472dd8620d364840d41f86c0857fbb2c167bbea8
Vignesh Muthukumar <vickymhs@gmail.com>
2022-11-10 22:52:26 -0500

    Added basic code for trust network

---
+++ src/trust_metric.py
@@ -0,0 +1,23 @@
+'''
+this file traverses through the processed dataset and takes a weighted sum of edges and components
+then find the top n time values
+if there are group of values present, normalize the edge weights and assign values
+show in networkx with the finalized version of graph
+'''
+def main():
+    # read through all the processed data
+
+    # iterate through each of the file
+
+    # for a single file, iterate through each row (each row will be a graph here)
+
+    # for every graph, compute the metric
+
+    # store in a dict with graph id (timestamp id) and the value of the metric
+
+    # sort the dict by value to get the top n timestamps
+
+    # aggregate the graphs in those range groups by performing NORMALIZATION
+
+if __name__ == "__main__":
+    main()
